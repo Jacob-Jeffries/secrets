cfe11173507860f3cf32e54ee52f982df75062ae
jacob@tiger <jeffrij85@gmail.com>
2024-11-12 18:16:55 -0500

    Finished the decrypt(), decryptFile(), and encryptFile() methods. The decrypted file is an excerpt from Anathem.

--- src/secrets/Secrets.java
+++ src/secrets/Secrets.java
@@ -8,16 +8,58 @@ import java.io.IOException;
 
 public class Secrets {
 
-    public static void encrypt(byte key, InputStream input, OutputStream output) throws IOException{
-      output.write(key);
-      int nextByte;
-      while((nextByte = input.read()) != -1 ){
-        output.write(key ^ nextByte);
+  public static void encrypt(byte key, InputStream input, OutputStream output) throws IOException{
+    output.write(key);
+    int nextByte;
+    while((nextByte = input.read()) != -1 ){
+      output.write(key ^ nextByte);
+    }
+  }
+
+  public static void decrypt(InputStream input, OutputStream output) throws IOException {
+    byte key = (byte)input.read();
+    int nextByte;
+    while ((nextByte = input.read()) != -1){
+      output.write((byte)(nextByte ^ key));
+    }
+  }
+
+  public static void decryptFile(String encryptedFile, String outFileName) throws IOException{
+    InputStream input = null;
+    OutputStream output = null;
+    try {
+      input = new FileInputStream(encryptedFile);
+      output = new FileOutputStream(outFileName);
+      decrypt(input, output);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    } finally {
+      if(input != null){
+        input.close();
+      }
+      if(output != null){
+        output.close();
       }
     }
+  }
 
-    public static void decrypt(InputStream input, OutputStream output) throws IOException {
-
+  public static void encryptFile(byte key, String plainFile, String outFileName) throws IOException{
+    InputStream input = null;
+    OutputStream output = null;
+    try {
+      input = new FileInputStream(plainFile);
+      output = new FileOutputStream(outFileName);
+      encrypt(key, input, output);
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+    } finally {
+      if(input != null){
+        input.close();
+      }
+      if(output != null){
+        output.close();
+      }
     }
+  }
 
 }
